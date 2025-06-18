f78a761cb64da839f0a50cb29c5ebe943c4fed34
Taynis910 <mehmet_tufan910@hotmail.com>
2025-06-18 12:42:46 +0300

    GlobalHandler sınıfı genel ve özel hataları yakalayacak şekilde düzenlendi.

--- src/main/java/com/mehmett/exception/ErrorType.java
+++ src/main/java/com/mehmett/exception/ErrorType.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorType {
 	
-	PASSWORD_MISMATCH(400, "Girilen şifreler uyuşmuyor.", HttpStatus.BAD_REQUEST);
+	PASSWORD_MISMATCH(5001, "Girilen şifreler uyuşmuyor.", HttpStatus.BAD_REQUEST),
+	VALIDATION_ERROR (400, "Girilen parametreler hatalıdır..",HttpStatus.BAD_REQUEST),
+	INVALID_SERVER_ERROR (500,"Sunucuda beklenmeyen hata",HttpStatus.INTERNAL_SERVER_ERROR);
 	
 	private int code;
 	private String message;
--- src/main/java/com/mehmett/exception/GlobalExceptionHandler_x.java
+++ src/main/java/com/mehmett/exception/GlobalExceptionHandler_x.java
@@ -1,24 +1,59 @@
 package com.mehmett.exception;
 
 import com.mehmett.dto.response.BaseResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler_x {
+	/**
+	 * Tanımlaması yapılmayan tüm hataları yakalamak için RuntimeException
+	 */
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<ErrorMessage> runtimeExceptionHandler(RuntimeException exception){
+		
+		return createResponseEntity(ErrorType.INVALID_SERVER_ERROR,HttpStatus.INTERNAL_SERVER_ERROR,null);
+	}
+	
+	
 	@ExceptionHandler(ProjectException.class) // İstisnayı yakalayan kısım
 	@ResponseBody
 	public ResponseEntity<ErrorMessage> projectException (ProjectException exception){
 		// ResponseEntity.ok() --> 200 OK. success her şey yolunda
 		// ResponseEntity.badRequest() --> 400 gelen istek hatalı
 		// ResponseEntity.internalServerError() --> Sunucu tarafında bir hata oluştu
-		return new ResponseEntity<>(ErrorMessage.builder()
-				                            .success(false)
-				                            .message(exception.getErrorType().getMessage())
-				                            .code(exception.getErrorType().getCode())
-				                            .build(), exception.getErrorType().getHttpStatus());
+		return createResponseEntity(exception.getErrorType(),exception.getErrorType().getHttpStatus(), null);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseBody
+	public ResponseEntity<ErrorMessage> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException exception){
+		List<String> fields = new ArrayList<>();
+		exception.getBindingResult().getFieldErrors().forEach(
+		// f.getField() -> Hata fırlatan nesnenin değişken adı
+		// f.getDefaultMessage() -> Hataya ait detay bilgisi
+				f-> fields.add("Değişken adı..: "+f.getField()+" - Hatanın Detayı..: "+f.getDefaultMessage())
+		);
+		return createResponseEntity(ErrorType.VALIDATION_ERROR,HttpStatus.BAD_REQUEST,fields);
+	}
+	
+	public ResponseEntity<ErrorMessage> createResponseEntity (ErrorType errorType,HttpStatus httpStatus,
+	            
+	                                                          List<String> errField){
+	log.error("TÜM HATALARIN GETİĞİ NOKTA: "+ errField);
+		return 	new ResponseEntity<>(ErrorMessage.builder()
+			                               .success(false)
+			                               .message(errorType.getMessage())
+			                               .fields(errField)
+			                               .build(),httpStatus);
 	}
 }
